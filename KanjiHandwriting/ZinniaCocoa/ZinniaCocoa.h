ba695841bc240d4b9faf6382ea169807d3dd5b9d
hunguet123 <hungk19elqd@gmail.com>
2025-04-03 06:16:39 +0700

    revert zinnia

---
+++ KanjiHandwriting/ZinniaCocoa/ZinniaCocoa.h
@@ -0,0 +1,24 @@
+//
+//  ZinniaCocoa.h
+//  ZinniaCocoa
+//
+//  Created by Morten Bertz on 5/15/15.
+//  Copyright (c) 2015 Morten Bertz. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ZinniaCocoa.
+FOUNDATION_EXPORT double ZinniaCocoaVersionNumber;
+
+//! Project version string for ZinniaCocoa.
+FOUNDATION_EXPORT const unsigned char ZinniaCocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZinniaCocoa/PublicHeader.h>
+
+
+#import <ZinniaCocoa/Recognizer.h>
+#import <ZinniaCocoa/Result.h>
+#import <ZinniaCocoa/Trainer.h>
+#import <ZinniaCocoa/Recognizer+MacOS.h>
